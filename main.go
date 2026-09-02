2b8471740eb6075ed9b4df8a2f9f83467b3ff6c9
aHereHere <96263174+aHereHere@users.noreply.github.com>
2026-09-03 00:30:09 +0530

    Support hosting platform port

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 	"time"
@@ -242,7 +243,11 @@ func main() {
 	mux.HandleFunc("OPTIONS /api/trips", withCORS(func(w http.ResponseWriter, r *http.Request) {}))
 	mux.HandleFunc("OPTIONS /api/trips/{id}/places", withCORS(func(w http.ResponseWriter, r *http.Request) {}))
 
-	addr := ":8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := ":" + port
 	log.Printf("place-review backend listening on %s", addr)
 	if err := http.ListenAndServe(addr, mux); err != nil {
 		log.Fatal(err)
